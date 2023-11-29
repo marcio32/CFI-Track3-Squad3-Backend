f505c35cc906e3748ac4edd9a5b47d1d13bedfcb
Mathias Perez <mathiasdavid@live.ca>
2023-11-29 19:40:47 -0300

    T3S3-10
    Se agregan Entites

---
+++ CFI-Track3-Squad3-Backend/CFI-Track3-Squad3-Backend/Entites/Users.cs
@@ -0,0 +1,26 @@
+﻿using CFI_Track3_Squad3_Backend.DTOs;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CFI_Track3_Squad3_Backend.Entites
+{
+    [Table("Users")]
+    public class Users
+    {
+        [Column("Id")]
+        public int Id { get; set; }
+        [Column("FirstName", TypeName = "VARCHAR(100)")]
+        public string? FirstName { get; set; }
+        [Column("LastName", TypeName = "VARCHAR(100)")]
+        public string? LastName { get; set; }
+        [Column("Password", TypeName = "VARCHAR(100)")]
+        public string? Password { get; set; }
+        [Column("Email", TypeName = "VARCHAR(100)")]
+        public string? Email { get; set; }
+        [Column("Points", TypeName = "NUMBER")]
+        public int Points { get; set; }
+        [Key]
+        [Column("RolId")]
+        public int RolId { get; set; }
+    }
+}
